0b837c9623838bcafc4672c51cc9ffd2bdae904b
rajeshyarava333 <93302362+rajeshyarava333@users.noreply.github.com>
2022-01-03 19:40:25 +0530

    first commit

---
+++ src/com/company/BinarySearch.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int key=40;
+        int result= Arrays.binarySearch(arr,key);
+        if(result<0){
+            System.out.println("Element is not found");
+        }
+        else{
+            System.out.println("Element is found at index "+result);
+        }
+    }
+}
